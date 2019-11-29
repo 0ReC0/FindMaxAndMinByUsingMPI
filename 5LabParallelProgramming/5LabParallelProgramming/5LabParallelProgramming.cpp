ce8db7fc3735f137f27172f20fef8cb125ececbd
ReC <kitanin.ser@mail.ru>
2019-11-29 20:11:24 +0300

    Added project

---
+++ 5LabParallelProgramming/5LabParallelProgramming/5LabParallelProgramming.cpp
@@ -0,0 +1,232 @@
+﻿// 2017.09.04
+// One of the more interesting Reduce options are MAXLOC and MINLOC.  They not only will
+// return a maximum or minimum value, but also which process had the corresponding value.
+// The Barrier function is also introduced here so that things get printed out in a nice
+// neat fashion.
+
+
+// Do not run this on too many processes, as it will be hard to visually veriy the results
+// of the MAXLOC and MINLOC reduce functions.
+// qsub -q computeLocalHD.q -pe mpi 5 subfile.qsub MPI_reduce_array_maxloc_example
+
+#include <math.h>
+#include <iostream>
+#include "mpi.h"
+#include <cstdlib>
+#include <time.h>
+#include <iomanip>
+using namespace std;
+
+int** InArray(int size, int** array)
+
+{
+	cout << "Enter array of " << size << "x" << size << " :" << endl;
+	for (int i = 0; i < size; i++) {
+
+		for (int j = 0; j < size; j++) {
+			cout << "Enter [" << i << "][" << j << "]:";
+			int element;
+			cin >> element;
+
+			array[i][j] = element;
+
+		}
+
+	}
+
+	return array;
+}
+void GenArray(int size, int** array)
+
+{
+
+	for (int i = 0; i < size; i++) {
+
+
+		for (int j = 0; j < size; j++) {
+
+			array[i][j] = rand();
+
+		}
+
+	}
+
+
+}
+void OutArray(int size, int** array)
+
+{
+
+	for (int i = 0; i < size; i++) {
+
+		for (int j = 0; j < size; j++) {
+
+			cout << setw(8) << array[i][j];
+
+		}
+
+		cout << endl;
+
+	}
+
+	cout << endl;
+
+}
+double getTimeSeq(double& timeEnd,
+	double& timeStart,
+	int& maxvalueSeq,
+	int& minvalueSeq,
+	int** arr,
+	int arraySize) {
+	timeStart = MPI_Wtime();
+
+	maxvalueSeq = minvalueSeq = arr[0][0];
+
+	for (int i = 0; i < arraySize; i++) {
+		for (int j = 0; j < arraySize; j++) {
+			if (arr[i][j] < minvalueSeq) {
+				minvalueSeq = arr[i][j];
+			}
+			if (maxvalueSeq < arr[i][j]) {
+				maxvalueSeq = arr[i][j];
+			}
+		}
+	}
+
+	timeEnd = MPI_Wtime();
+
+	return timeEnd - timeStart;
+}
+void TestArray(int** arr, int arraySize, int rank, int size) {
+	int maxvalue = 0;
+	int minvalue = 0;
+	int maxvalueSeq = 0;
+	int minvalueSeq = 0;
+	int globalmaxvalue = 0;
+	int globalminvalue = 0;
+
+	double timeStart, timeEnd;
+	double timeSeq;
+
+
+	if (rank == 0) {
+
+		timeSeq = getTimeSeq(timeEnd, timeStart, maxvalueSeq, minvalueSeq, arr, arraySize);
+
+	}
+
+	maxvalue = minvalue = arr[0][0];
+
+	if (rank == 0) {
+		timeStart = MPI_Wtime();
+	}
+	for (int i = 0; i < arraySize; i++) {
+		for (int j = 0; j < arraySize; j++) {
+			if (arr[i][j] < minvalue) {
+				minvalue = arr[i][j];
+			}
+			if (maxvalue < arr[i][j]) {
+				maxvalue = arr[i][j];
+			}
+		}
+	}
+
+	MPI_Reduce(&maxvalue, &globalmaxvalue, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&minvalue, &globalminvalue, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+
+	if (rank == 0) {
+		timeEnd = MPI_Wtime();
+	}
+	if (rank == 0)
+	{
+		cout << "NumThreads: " << size << "\t Array size: " << arraySize << endl
+			<< "Max Seq: " << setw(10) << maxvalueSeq << "\t Max Parallel: " << globalmaxvalue << endl
+			<< "Min Seq: " << setw(10) << minvalueSeq << "\t Min Parallel: " << globalminvalue << endl
+			<< "timeSeq: " << setw(11) << timeSeq  << "\t timeParallel: " << setw(11)<< timeEnd - timeStart
+			<<"\t speed-up: " << setw(10) << timeSeq / (timeEnd - timeStart) << "\t cost: " << setw(10) << (timeEnd - timeStart) * size
+			<< "\t efficiency:" << setw(10) << timeSeq / ((timeEnd - timeStart) * size) << endl << endl;
+		;
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+}
+
+void runControlTests(int rank, int size) {
+	int sizes[3] = { 100, 500, 1000 };
+
+	for (int arrSize : sizes)
+	{
+		int** array = new int* [arrSize];
+
+		for (int i = 0; i < arrSize; i++) {
+
+			array[i] = new int[arrSize];
+		}
+		if (rank == 0) {
+			GenArray(arrSize, array);
+
+			for (int i = 0; i < arrSize; i++)
+			{
+				MPI_Bcast(&(array[i][0]), arrSize, MPI_INT, 0, MPI_COMM_WORLD);
+			}
+		}
+		else {
+			for (int i = 0; i < arrSize; i++)
+			{
+				MPI_Bcast(&(array[i][0]), arrSize, MPI_INT, 0, MPI_COMM_WORLD);
+			}
+		}
+
+		MPI_Barrier(MPI_COMM_WORLD);
+		TestArray(array, arrSize, rank, size);
+	}
+}
+
+void runAlgorithmTestWithUserInput(int rank, int size, int arrSize) {
+
+	int** array = new int* [arrSize];
+
+	for (int i = 0; i < arrSize; i++) {
+
+		array[i] = new int[arrSize];
+	}
+	if (rank == 0) {
+		InArray(arrSize, array);
+
+		for (int i = 0; i < arrSize; i++)
+		{
+			MPI_Bcast(&(array[i][0]), arrSize, MPI_INT, 0, MPI_COMM_WORLD);
+		}
+	}
+	else {
+		for (int i = 0; i < arrSize; i++)
+		{
+			MPI_Bcast(&(array[i][0]), arrSize, MPI_INT, 0, MPI_COMM_WORLD);
+		}
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	if (rank == 0) {
+		OutArray(arrSize, array);
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	TestArray(array, arrSize, rank, size);
+}
+
+int main()
+{
+	int rank, size; // standard for each process
+
+	MPI_Init(NULL, NULL); // initialize the MPI world
+	MPI_Comm_size(MPI_COMM_WORLD, &size); // get total size of the world
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank); // each process gets its own rank
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	runAlgorithmTestWithUserInput(rank, size, 5);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	runControlTests(rank, size);
+
+	MPI_Finalize();
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-FindMaxAndMinByUsingMPI
+# FindMaxAndMinByUsingMPI
+## How to run
+1) Download Microsoft MPI v10.0 https://www.microsoft.com/en-us/download/details.aspx?id=57467
+2) Install SDK and MPI setup
+3) Set up MS MPI https://www.math.ucla.edu/~mputhawala/PPT.pdf
+3) Build project in Visual Studio
+4) Run 5LabParallelProgramming.exe by using powershell command `mpiexec -n 4 ./5LabParallelProgramming.exe`, where 4 it is number of threads
